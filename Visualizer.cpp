4f80c8eebd42435ca2a52386c07d02e50acc40dc
ashishkamblecctech <149309502+ashishkamblecctech@users.noreply.github.com>
2023-12-15 19:09:32 +0530

    Update Visualizer.cpp

--- Visualizer.cpp
+++ Visualizer.cpp
@@ -17,19 +17,6 @@ Visualizer::~Visualizer()
 {}
 
 void Visualizer::setupUi() {
-   /* resize(600, 400);
-    mMenuBar = new QMenuBar(this);
-    mMenuBar->setObjectName("menuBar");
-    setMenuBar(mMenuBar);
-    mMainToolBar = new QToolBar(this);
-    mMainToolBar->setObjectName("mainToolBar");
-    addToolBar(mMainToolBar);
-    mStatusBar = new QStatusBar(this);
-    mStatusBar->setObjectName("statusBar");
-    setStatusBar(mStatusBar);
-    mRenderer = new OpenGLWindow(QColor(0,0,0), this);
-    setCentralWidget(mRenderer);
-    setWindowTitle(QCoreApplication::translate("Z-Buffer", "Z-Buffer", nullptr));*/
     resize(600, 400);
     centralWidget = new QWidget(this);
     centralWidget->setObjectName("centralWidget");
